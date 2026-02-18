3dbf0203bd7778436e5c0d25f62148e862eb92d3
Anna Lucya Gabriel <annalucyagabriel@gmail.com>
2026-02-18 20:13:15 +0530

    suggester added

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -3,7 +3,11 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 MODEL_PATH = "../trained_model"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+tokenizer = AutoTokenizer.from_pretrained(
+    MODEL_PATH,
+    use_fast=False
+)
+
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
 model.eval()
 
--- backend/database.py
+++ backend/database.py
@@ -1,29 +1,13 @@
-import sqlite3
-import os
-os.makedirs("../database", exist_ok=True)
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+DB_CONFIG = {
+    "dbname": "vulner_db",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432
+}
 
 def get_connection():
-    return sqlite3.connect("../database/files.db")
-
-
-def create_table():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS uploaded_files (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        filename TEXT,
-        filepath TEXT,
-        result TEXT,
-        uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    """)
-
-    conn.commit()
-    conn.close()
-
-if __name__ == "__main__":
-    create_table()
-    print("Database and table created successfully.")
+    return psycopg2.connect(**DB_CONFIG)
